4bb1a4d5c8f36476bf3dada1ebf49a46e41c3043
Patrick Simpson <izerop@gmail.com>
2016-06-15 14:49:48 -0400

    feat: add support for gem removal

--- bin/fetch-gems.rb
+++ bin/fetch-gems.rb
@@ -35,21 +35,36 @@ class GemGetter
     messages = data["messages"]
     puts "We have #{messages.count} messages starting at '#{ts}' time"
     messages.each do |message|
+      has_public = false
+      disapproved = false
+      start_text = ""
+      text = ""
       if reactions = message["reactions"]
         reactions.each do |r|
           if  /^earth_*/.match(r["name"])
+            has_public = true
             user_name = $users[message["user"]]
             text = message["text"].inspect
-
+            if user_name == "gem-me-bot"
+              start_text = ""
+            else
+              start_text = "#{user_name}: "
+            end
             if users_mentioned = text.scan(/\<\@(U[A-Z0-9]+)\>/)
               users_mentioned.each do |u|
                 user_mention = $users[u[0]]
+                puts user_mention
                 text = text.sub("<@#{u[0]}>", "@#{user_mention}")
               end
             end
-            $messages.push("#{user_name}: #{text}")
-            puts "gem count #{$messages.count}"
           end
+          if  /^x/.match(r["name"])
+            disapproved = true
+          end
+        end
+        if has_public and !disapproved
+          $messages.push("#{start_text}#{text}")
+          puts "gem count #{$messages.count}"
         end
       end
     $last_ts = message["ts"]
